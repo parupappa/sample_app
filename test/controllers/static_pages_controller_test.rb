ce87f080223ce6e44a1498e226388a4596f4b3d7
横尾杏之介 <yokoo.an209@gmail.com>
2021-02-05 09:16:57 +0900

    Test success

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # Homeページのテスト。GETリクエストをhomeアクションに対して発行（=送信）せよ。
+  # そうすれば、リクエストに対するレスポンスは［成功］になるはず」
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -8,6 +10,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
